dd99a5fcc7eaf553d1a5afd73ad3dcb8a89cd388
naveen-2111-dev <naveenrajanm9@gmail.com>
2025-03-20 09:55:10 +0530

    server error

--- server/utils/board.js
+++ server/utils/board.js
@@ -1,11 +1,10 @@
 const ConnectConfig = require("../lib/Connect.config");
 
-async function insertLeaderboardData(wallet, username, score = 0) {
+async function insertLeaderboardData(collection, wallet, username, score = 0) {
   try {
-    const db = await ConnectConfig();
-    const collection = db.leaderboard;
+  
     const leaderboardData = {
-      wallet,
+      wallet, 
       username,
       score,
       timestamp: new Date(),
@@ -13,11 +12,11 @@ async function insertLeaderboardData(wallet, username, score = 0) {
 
     const result = await collection.insertOne(leaderboardData, {
       maxTimeMS: 5000,
-    }); 
+    });
     return result;
   } catch (err) {
     console.error("Error inserting leaderboard data:", err);
-    throw err; 
+    throw err;
   }
 }
 
